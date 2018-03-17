5eb0ca24e02b439319f41172952cc52a2122b6aa
Reinert <reilemx@gmail.com>
2018-03-17 18:39:56 +0100

    Status code test improvements

--- tests/server/StatusCodeServerTests.java
+++ tests/server/StatusCodeServerTests.java
@@ -9,14 +9,22 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StatusCodeServerTests {
 
     private HTTPServer server;
 
+    private ByteArrayOutputStream outContent;
+
     @BeforeEach
     void serverSetup() {
+        this.outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
         this.server = new HTTPServer(ServerTestUtil.PORT);
         this.server.start();
     }
@@ -33,6 +41,7 @@ class StatusCodeServerTests {
         ServerTestUtil.createAndExecuteClient(
                 HTTPMethod.GET, "localhost/", HTTPProtocol.HTTP_1_1, null, extra
         );
+        assertTrue(outContent.toString().contains("304 Not Modified\r\n"));
     }
 
     @Test
@@ -42,6 +51,7 @@ class StatusCodeServerTests {
         ServerTestUtil.createAndExecuteClient(
                 HTTPMethod.GET, "localhost/", HTTPProtocol.HTTP_1_1, null, extra
         );
+        assertTrue(outContent.toString().contains("400 Bad Request\r\n"));
     }
 
     @Test
@@ -49,6 +59,7 @@ class StatusCodeServerTests {
         ServerTestUtil.createAndExecuteClient(
                 HTTPMethod.PUT, "localhost/", HTTPProtocol.HTTP_1_1, "Overwrite the root! >:D", null
         );
+        assertTrue(outContent.toString().contains("403 Forbidden\r\n"));
     }
 
     @Test
@@ -56,6 +67,7 @@ class StatusCodeServerTests {
         ServerTestUtil.createAndExecuteClient(
                 HTTPMethod.GET, "localhost/a_file_that_does_not_exist.txt", HTTPProtocol.HTTP_1_1, null, null
         );
+        assertTrue(outContent.toString().contains("404 Not Found\r\n"));
     }
 
     @Test
@@ -65,6 +77,7 @@ class StatusCodeServerTests {
         ServerTestUtil.createAndExecuteClient(
                 HTTPMethod.PUT, "localhost/test.txt", HTTPProtocol.HTTP_1_1, "Put this text.\n", extra
         );
+        assertTrue(outContent.toString().contains("411 Length Required\r\n"));
     }
 
     @Test
@@ -72,15 +85,19 @@ class StatusCodeServerTests {
         ServerTestUtil.createAndExecuteClient(
                 HTTPMethod.OPTIONS, "localhost", HTTPProtocol.HTTP_1_1, null, null
         );
+        assertTrue(outContent.toString().contains("501 Not Implemented\r\n"));
         ServerTestUtil.createAndExecuteClient(
                 HTTPMethod.DELETE, "localhost", HTTPProtocol.HTTP_1_1, null, null
         );
+        assertTrue(outContent.toString().contains("501 Not Implemented\r\n"));
         ServerTestUtil.createAndExecuteClient(
                 HTTPMethod.TRACE, "localhost", HTTPProtocol.HTTP_1_1, null, null
         );
+        assertTrue(outContent.toString().contains("501 Not Implemented\r\n"));
         ServerTestUtil.createAndExecuteClient(
                 HTTPMethod.CONNECT, "localhost", HTTPProtocol.HTTP_1_1, null, null
         );
+        assertTrue(outContent.toString().contains("501 Not Implemented\r\n"));
     }
 
     @Test
@@ -88,9 +105,11 @@ class StatusCodeServerTests {
         ServerTestUtil.createAndExecuteClient(
                 HTTPMethod.GET, "localhost", HTTPProtocol.HTTP_0_9, null, null
         );
+        assertTrue(outContent.toString().contains("505 HTTP Version Not Supported\r\n"));
         ServerTestUtil.createAndExecuteClient(
                 HTTPMethod.GET, "localhost", HTTPProtocol.HTTP_2_0, null, null
         );
+        assertTrue(outContent.toString().contains("505 HTTP Version Not Supported\r\n"));
     }
 
 }
